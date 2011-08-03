80393c3b293d1a4dbd2638706418d948e612b6c2
jade <zjhiphop@gmail.com>
2011-08-03 20:48:28 +0800

    1.add expand method for native method 2.fix utility issue

---
+++ Tools/blitz.test.js
@@ -0,0 +1,13 @@
+//Create a new object
+/**
+dependency of blitz.utility.js
+*/
+!function($){
+    blitz.test={
+     equal:function(a,b){
+       return a.compare(b);
+     }
+    };
+	
+}(jQuery)
+
--- Tools/blitz.utility.js
+++ Tools/blitz.utility.js
@@ -1,46 +1,55 @@
-//Create a new object
-!function($){
-    blitz.utility=utility={
+//Blitz tools Utility
+(function($){
+    'use strict'
+	var _userAgent=(navigator||window.navigator).userAgent;
+    blitz.utility={
 	  log:function(msg){
 	    if(console.log){
 		  console.log(msg);
+		}else if(window.status){
+          window.status=msg;
+        }else{
+          $('title').text($('title').text()+' log:'+msg);
+        }
+	  },
+	  createStyleElemnt:function(innerhtml){
+		if(document.all){
+			window.style=innerhtml;
+			document.createStyleSheet("javascript:style");
+		}else{
+			var style = document.createElement('style');
+			style.type = 'text/css';
+			style.rel='stylesheet';
+			style.innerHTML=innerhtml;
+			document.getElementsByTagName('HEAD')[0].appendChild(style);
 		}
+	  },
+	  config:{
+	  	storageCss:" {behavior:url(#default#userData);}"
+	  },
+	  /*browser detect*/
+	  isIE:/*@cc_on!@*/0,
+	  isIE6:/ie 6/ig.test(_userAgent),
+	  isIE7:/ie 7/ig.test(_userAgent),
+	  isIE8:/ie 8/ig.test(_userAgent),
+	  isIE9:/ie 9/ig.test(_userAgent),
+	  isIpad:/ipad/ig.test(_userAgent),
+	  isChrome:/chrome/ig.test(_userAgent),
+	  isFirefox:/firefox/ig.test(_userAgent),
+	  isOpera:/opera/ig.test(_userAgent),
+	  /*end browser detect*/
+	  cssCheck:function (cssText) {
+	  /*check wheither insist the specific style,eg: backgroundSize */
+		   var style = document.createElement('cssText').style, domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+		   ucProp = cssText.charAt(0).toUpperCase() + cssText.substr(1),
+		   props = (cssText + ' ' + domPrefixes.join(ucProp + ' ') + ucProp).split(' ');
+			for (var i in props) {
+				if (style[props[i]] !== undefined) {
+					return true;
+				}
+			}
+			return false;
 	  }
 	}
-	var f=Function.prototype;
-	/*
-	*used to creat a new object
-	*/
-	Object.create=Object.create||function(o){
-			   var f=function(){};
-			   f.prototype=(o.prototype||o||{});
-			   return new f();
-    };
-	Object.isArray=Array.isArray||function(){
-	     return ~Object.constructor.toString().indexOf("Array"); 
-	}
-	
-	/*
-	*decration mode
-	*eg: f1.prototype.opa=fn2.prototype.add
-	*    f1.opa(1) is equivalent to fns.add(1)
-	*/
-	f.partial = function() {
-		var fn = this,
-		args=$?$.makeArray(arguments):[].slice.call(arguments);//if jquery is used,then will use jquery lib at first
-		return function() {
-			return fn.apply(this,args.concat(arguments));
-		};
-	};
-	/*
-	*function chained
-	*eg: function a(){}
-	*
-	*/
-	f.method=function(name,func){
-	  this[name]=func;
-	  return this;
-	}
-	
-}(jQuery)
+})(jQuery)
 
